01207161a33abafb5a8fb782a5bb257c4a04ebb8
Kartikeya Mishra <kmishra@umd.edu>
2022-03-06 14:28:02 -0500

    Add pdoc comments to runner file

--- runner.py
+++ runner.py
@@ -1,13 +1,19 @@
+"""main file
+"""
+
 #!/usr/bin/env python3
 # Author @ Kartikeya Mishra
 
 from planner import Planner
 from utility import ask_for_order
-    
+
 def main_func():
+    """This is the starting point of the program
+    """
     planner = Planner()
     order = ask_for_order(planner)
     planner.generate_plan(order)
 
 if __name__ == '__main__':
     main_func()
+    
